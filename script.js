356b8eae33347fc7e99aeff922d1156e5b508c47
Faith <ardist456@gmail.com>
2025-01-19 17:07:07 -0500

    Update script.js
    fixed it

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //↓↓↓↓↓↓↓↓↓↓↓↓TROUBLESHOOT the code  below here↓↓↓↓↓↓↓↓↓↓↓↓
 const mySuperNeatoObject = {
-    type: "gadget"
-    hasWiFi: false
-    }
-
+    type: "gadget",
+    hasWifi: false,
 }
+
+
 //↑↑↑↑↑↑↑↑↑↑TROUBLESHOOT the code above here↑↑↑↑↑↑↑↑↑↑
 //Do not edit below this comment!
 
